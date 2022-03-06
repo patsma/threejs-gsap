e35bddab241549831c16c5a85b0cb24f72bf9dfe
Patryk Smakosz <patryksmakosz1@gmail.com>
2022-03-06 05:34:27 +0100

    refactor: artist-x first effect

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-import './style.css'
+import './style.scss'
 import * as THREE from 'three';
-import { gsap } from "gsap";
+import {gsap} from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
 
+gsap.registerPlugin(ScrollTrigger);
+/*
 console.log('works')
 
 const scene = new THREE.Scene();
@@ -19,11 +22,54 @@ scene.add( cube );
 camera.position.z = 5;
 function animate() {
     requestAnimationFrame( animate );
-    gsap.timeline()
-        .to(cube.rotation, {x: '-=0.1'})
-        .to(cube.rotation, {y: '-=0.1'},'<')
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
+    // gsap.timeline()
+    //     .to(cube.rotation, {x: '-=0.1'})
+    //     .to(cube.rotation, {y: '-=0.1'},0)
+        // .to(cube.position, {z: '+=0.1'},0)
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
     renderer.render( scene, camera );
 }
 animate();
+const tl = gsap.timeline()
+tl
+    .to(cube.rotation, {x: '-=0.4'})
+    .to(cube.rotation, {y: '-=0.4'},0)
+// .to(cube.position, {z: '+=0.1'},0)
+
+ScrollTrigger.create({
+    trigger:'body',
+    markers:true,
+    
+    start:'100px top',
+    end: "2000px 2000px",
+    scrub:8,
+    animation:tl
+})*/
+gsap.set('#Layer_1-2 > g > image', {x: '-=100', y: '+=550', transformOrigin: 'center center'})
+gsap.set('.hero', {scale: '+=0.3', transformOrigin: 'center center'})
+const tl = gsap.timeline({
+    defaults: {
+        ease: 'linear'
+    },
+    scrollTrigger: {
+        trigger: 'body',
+        pin: '.hero',
+        // markers: true,
+        scrub: true,
+        duration: '200%'
+
+    }
+})
+tl
+    .to('#mask *', {scaleY: '+=8', scaleX: '+=10', transformOrigin: 'center center'})
+    // .to('#mask > rect', {scaleY: '+=8', scaleX: '+=8', transformOrigin: 'center center'})
+    // .to('#mask > rect', {scaleX: '+=14', transformOrigin: 'center center'},'-=0.4')
+    .to('#Layer_1-2 > g > image', {
+        scale: '+=1.5',
+        x: '+=10',
+        y: '+=10',
+        transformOrigin: 'center center',
+        ease: 'power1.out'
+    }, 0.1)
+
